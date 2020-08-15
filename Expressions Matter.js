562c7e8a3b73ac296ff61cd2ff2be140683de7a4
Umidjohn <umidjohn@mail.ru>
2020-08-15 23:32:15 +0500

    add CodeWarsSolutions

---
+++ Expressions Matter.js
@@ -0,0 +1,12 @@
+function expressionMatter(a, b, c) {
+    let br1 = a * (b + c);
+    let br2 = a * b * c;
+    let br3 = a + b * c;
+    let br4 = (a + b) * c;
+    if ( a == 1 && c == 1 ) return a + b + c;
+    if ( br1 >= br2 && br1 >= br3 && br1 >= br4) return br1;
+    if ( br2 >= br1 && br2 >= br3 && br2 >= br4) return br2;
+    if ( br3 >= br1 && br3 >= br2 && br3 >= br4) return br3;
+    if ( br4 >= br1 && br4 >= br2 && br4 >= br3) return br4;
+}
+// https://www.codewars.com/kata/5ae62fcf252e66d44d00008e
